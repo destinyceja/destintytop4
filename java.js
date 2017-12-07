8934f3469492305df5705e476d97b3b7ba8084bc
destinyceja <33135802+destinyceja@users.noreply.github.com>
2017-12-07 13:51:53 -0800

    Create java.js

---
+++ java.js
@@ -0,0 +1,12 @@
+function play1(){
+documnet.getElementById("strangerthings").play();
+}
+function play2(){
+documnet.getElementById("americanhorror").play();
+}
+function play3(){
+documnet.getElementById("riverdale").play();
+}
+function play4(){
+documnet.getElementById("thegetdown").play();
+}
