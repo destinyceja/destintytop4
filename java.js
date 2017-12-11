73b6adc9cff24b9679750541a5c4d58356d987ea
destinyceja <33135802+destinyceja@users.noreply.github.com>
2017-12-11 13:50:11 -0800

    Update java.js

--- java.js
+++ java.js
@@ -1,12 +1,25 @@
 function play1(){
 document.getElementById("strangerthings").play();
+document.getElementById("americanhorror").pause();
+   document.getElementById("riverdale").pause();
+   document.getElementById("thegetdown").pause();
 }
 function play2(){
 document.getElementById("americanhorror").play();
+  document.getElementById("strangerthings").pause();
+   document.getElementById("riverdale").pause();
+   document.getElementById("thegetdown").pause();
 }
 function play3(){
 document.getElementById("riverdale").play();
+  document.getElementById("americanhorror").pause();
+   document.getElementById("strangerthings").pause();
+   document.getElementById("thegetdown").pause();
 }
 function play4(){
 document.getElementById("thegetdown").play();
+  document.getElementById("americanhorror").pause();
+   document.getElementById("riverdale").pause();
+   document.getElementById("strangerthings").pause();
 }
+
