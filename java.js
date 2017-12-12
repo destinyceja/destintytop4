03b4b917bf8f509b4f76a282600f029ad53fa5b6
destinyceja <33135802+destinyceja@users.noreply.github.com>
2017-12-12 12:40:56 -0800

    Update java.js

--- java.js
+++ java.js
@@ -1,21 +1,32 @@
+//Plays function by finding the ID in the HTML File on the bottom of the code//
+//Pause funtion to restrict so that their only be one song playing at a time//
 function play1(){
 document.getElementById("strangerthings").play();
 document.getElementById("americanhorror").pause();
    document.getElementById("riverdale").pause();
    document.getElementById("thegetdown").pause();
 }
+
+//Plays function by finding the ID in the HTML File on the bottom of the code//
+//Pause funtion to restrict so that their only be one song playing at a time//
 function play2(){
 document.getElementById("americanhorror").play();
   document.getElementById("strangerthings").pause();
    document.getElementById("riverdale").pause();
    document.getElementById("thegetdown").pause();
 }
+
+//Plays function by finding the ID in the HTML File on the bottom of the code//
+//Pause funtion to restrict so that their only be one song playing at a time//
 function play3(){
 document.getElementById("riverdale").play();
   document.getElementById("americanhorror").pause();
    document.getElementById("strangerthings").pause();
    document.getElementById("thegetdown").pause();
 }
+
+//Plays function by finding the ID in the HTML File on the bottom of the code//
+//Pause funtion to restrict so that their only be one song playing at a time//
 function play4(){
 document.getElementById("thegetdown").play();
   document.getElementById("americanhorror").pause();
